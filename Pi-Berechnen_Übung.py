4e1fdaad578d279dcd941c688843b6f1f1bb3357
Timoh01 <80050903+Timoh01@users.noreply.github.com>
2021-04-29 14:45:52 +0200

    Add files via upload

---
+++ Pi-Berechnen_Übung.py
@@ -0,0 +1,9 @@
+# Pi - berechnen mit Leibniz-Reihe
+
+def Leibnizreihe(Anzahl, k=0, prev=0):
+    prev += ((-1)**k)/(2*k+1)
+    if k >= Anzahl-1:
+        return 4*prev
+    return Leibnizreihe(Anzahl, k+1, prev) 
+ 
+print(Leibnizreihe(Anzahl = int(input('Bitte geben Sie an, wie viele Brüche berechnet werden sollen:\n'))))
